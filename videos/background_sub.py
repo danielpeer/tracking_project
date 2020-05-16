5616a625a208251b415ef6025b9c259e083a8c40
Rweiser91 <roeyweiser@gmail.com>
2020-05-16 12:00:34 +0300

    adding the walking alone video

--- videos/background_sub.py
+++ videos/background_sub.py
@@ -2,9 +2,9 @@ import cv2
 import numpy as np
 from videos import *
 
-cap = cv2.VideoCapture(".\\..\\videos\\walking_alone.avi")
+cap = cv2.VideoCapture(".\\..\\videos\\Stabilized_Example_INPUT.avi")
 kernel = np.ones((5, 5), np.uint8)
-subtractor = cv2.createBackgroundSubtractorMOG2(history=20, varThreshold=200, detectShadows=True)
+subtractor = cv2.createBackgroundSubtractorMOG2(history=0, varThreshold=10, detectShadows=True)
 
 while True:
     scale_percent = 50
@@ -16,11 +16,11 @@ while True:
     gray = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2GRAY)
     mask = subtractor.apply(gray)
     #mask = cv2.morphologyEx(mask, cv2.MORPH_GRADIENT, kernel)
-    #mask = cv2.GaussianBlur(mask, (3, 3), 0)
-    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-    # mask = cv2.erode(mask, kernel, iterations=1)
+    mask = cv2.GaussianBlur(mask, (3, 3), 0)
+    #mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    mask = cv2.erode(mask, kernel, iterations=1)
     #mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-
+    cv2.threshold(mask, 140, 255, cv2.THRESH_BINARY, mask)
     cv2.imshow("Frame", resized_frame)
     cv2.imshow("mask", mask)
 

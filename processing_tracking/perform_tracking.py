f572036ae6ea49f1ee1434b1c54c55d39149a9b7
Rweiser91 <roeyweiser@gmail.com>
2020-05-16 19:46:04 +0300

    changing background substraction to be a median filter and then subtracting each frame from the 'background' fame. added the scaling frame function in perform_tracking_utilities.py

--- processing_tracking/perform_tracking.py
+++ processing_tracking/perform_tracking.py
@@ -7,6 +7,7 @@ from processing_tracking.state_machine import *
 from processing_tracking.target import *
 from processing_tracking.SearchWindow import *
 from processing_tracking.GUI import *
+from processing_tracking.perform_tracking_utilities import *
 from videos import *
 
 system_mode = "debug "
@@ -14,12 +15,11 @@ should_add_gaussian_noise = False
 
 
 def perform_tracking():
-    count = 0
-    kernel = np.ones((5, 5), np.uint8)
+
     if system_mode != "debug ":
         input_video = input("Please enter a video path:\n")
     else:
-        input_video = ".\\..\\videos\\walking_alone.mp4"
+        input_video = ".\\..\\videos\\walking.mp4"
     try:
         cap = cv2.VideoCapture(input_video)
         select_target_flag = False
@@ -30,6 +30,23 @@ def perform_tracking():
             print("Error opening video stream or file")
             return
 
+        # Randomly select 25 frames to create background for background subtraction
+        frameIds = cap.get(cv2.CAP_PROP_FRAME_COUNT) * np.random.uniform(size=25)
+
+        # Store selected frames in an array
+        frames = []
+        for fid in frameIds:
+            cap.set(cv2.CAP_PROP_POS_FRAMES, fid)
+            ret, frame = cap.read()
+            resized_frame = frame_scaling(frame)
+            frames.append(resized_frame)
+
+        # Calculate the median along the time axis
+        background = np.median(frames, axis=0).astype(dtype=np.uint8)
+        gray_background = cv2.cvtColor(background, cv2.COLOR_BGR2GRAY)
+        cap.release()
+        cap = cv2.VideoCapture(input_video)
+        cv2.imshow('gray_background', gray_background)
         # retrieving Resolution
         frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -41,33 +58,24 @@ def perform_tracking():
         out1 = cv2.VideoWriter('berlin_walk.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), fps,
                                (frame_width, frame_height))
         red = [0, 0, 255]
-        substractor = cv2.createBackgroundSubtractorMOG2(history=20, varThreshold=50, detectShadows=True)
+        #substractor = cv2.createBackgroundSubtractorMOG2(history=20, varThreshold=50, detectShadows=True)
 
         # Read until video is completed
         retries = 0
         while cap.isOpened():
-            scale_percent = 50  # percent of original size
             # Capture frame-by-frame
             ret, frame = cap.read()
 
             if ret:
                 # adjusting frame size to fit screen properly
-                width = int(frame.shape[1] * scale_percent / 100)
-                height = int(frame.shape[0] * scale_percent / 100)
-                dim = (width, height)
-                resized_frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+                resized_frame = frame_scaling(frame)
 
                 # converting to grayscale in order to calculate correlation and applying background substraction mask
                 gray = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2GRAY)
-                mask = substractor.apply(gray)
-                cv2.threshold(mask,2,255,cv2.THRESH_BINARY)
-                # background substraction needs a couple of frames to learn the target
-                if mask[0][0] == 127 or count < 5:
-                    count += 1
-                    continue
-                mask = cv2.morphologyEx(mask, cv2.MORPH_GRADIENT, kernel)
-                mask = cv2.GaussianBlur(mask, (3, 3), 0)
-                mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+
+                mask = cv2.absdiff(gray_background, gray)
+                _, mask = cv2.threshold(mask, 25, 255, cv2.THRESH_BINARY)
+
                 if not select_target_flag:  # creating the target only once
                     target_info = Target(resized_frame, mask)
                     search_window_info = SearchWindow(target_info)
@@ -94,8 +102,8 @@ def perform_tracking():
 
                 final_prediction = kalman.get_prediction(prediction)
                 x, y = final_prediction[0][0], final_prediction[1][0]
-                cv2.rectangle(resized_frame, (y - int(target_info.target_h/2), x - int(target_info.target_w/2)),
-                              (y + int(target_info.target_h/2), x + int(target_info.target_w/3)), red, 1)
+                cv2.rectangle(resized_frame, (y - int(target_info.target_h / 2), x - int(target_info.target_w / 2)),
+                              (y + int(target_info.target_h / 2), x + int(target_info.target_w / 3)), red, 1)
 
                 target_info.update_position(y, x)
                 state_holder.update_previous_pos((x, y))
